1eaa20dc36ba84215ae6221572883c22d1180c7b
jonvfvn <jonnym46@icloud.com>
2024-02-12 15:08:24 +0000

    quick layout

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
 import { Loading } from "@/components/auth/loading";
